99b10844f64ed6f83c78020bad837887b32afc75
Bilous <d818651@gmail.com>
2018-11-07 00:35:04 +0200

    alfa.01  Google History

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
 var time = 5;
-
+var timerId;
+var currentImg=0;
+var arrayOfImageSrs;
 function run() {
   var inputList = document.getElementById('inputList');
   var text = inputList.value;
-  var arrayOfImageSrs = text.split("\n");   
-  inputList.hidden = true;                  // скрываю текст от изменения
-  document.querySelector("h1").innerHTML = "<img id=\"img\" src=\"pic1.img\" alt=\"Image text\">"; // создаю новый элемент картинка
-  var img = document.getElementById("img");
-  img.src = "https://www.google.com.ua/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png";
+  arrayOfImageSrs = text.split("\n");
+  inputList.hidden = true; // скрываю текст от изменения
+  document.querySelector("h1").innerHTML = "<img id=\"img\" alt=\"Image text\">"; // создаю новый элемент картинка
+  changeTime();
+}
+
+function changeImg() {
+  // if (document.getElementById("img") != undefined) 
+  {
+    var img = document.getElementById("img");
+    img.src = arrayOfImageSrs[currentImg];
+    console.log(arrayOfImageSrs[currentImg]);
+    currentImg++;
+    if (currentImg>=arrayOfImageSrs.length-1) currentImg=0;
+  }
 
 }
 
 function changeTime() {
   var val = document.getElementById("time").value;
-  if (!isNaN(val)){
+  if (!isNaN(val)) {
     time = +val;
-    document.getElementById("time").style="font-size: 35px"; // change style
+    document.getElementById("time").style = "font-size: 18px"; // change style
+    clearInterval(timerId);
+    changeImg();
+    timerId = setInterval(changeImg, time * 1000);
   } else alert("False time");
-}
- 
-function changeImg() {
-  if (document.getElementById("img") != undefined) {
-  }
 }
